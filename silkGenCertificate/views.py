ce48768d6d92c44c2dcf8af3ab9855bb44b67f29
mishal shanavas <mishalshanavas8@gmail.com>
2025-06-07 14:54:06 +0530

    UPDATE: Fixed Space issue now

--- silkGenCertificate/views.py
+++ silkGenCertificate/views.py
@@ -2,13 +2,14 @@ from django.http import FileResponse
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from urllib.parse import unquote
 from pypdf import PdfReader, PdfWriter
 import os
 from io import BytesIO
 
 def ciscoCyberSec(request, name, date_str):
-    name = name.replace('%20', ' ')
-    date_str = date_str.replace('%20', ' ')
+    name = unquote(name)
+    date_str = unquote(date_str)
     template_path = os.path.join(os.path.dirname(__file__), 'CertificateTemplate', 'cisco_cyber_security.pdf')
     font_path = os.path.join(os.path.dirname(__file__), 'CertificateFonts', 'ArialBold.TTF')
     pdfmetrics.registerFont(TTFont('ArialBold', font_path))
